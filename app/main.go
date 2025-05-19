e6a04f567ebed5d02ac61399492be37b5f5d2754
HuaiguangYe <greenar@gmail.com>
2025-05-19 21:27:02 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type RESTPrefix string
+
+const (
+	arrayPrefix        RESTPrefix = "*"
+	bulkStringPrefix   RESTPrefix = "$"
+	simpleStringPrefix RESTPrefix = "+"
+	errorPrefix        RESTPrefix = "-"
+	integerPrefix      RESTPrefix = ":"
+)
+
+func (p RESTPrefix) String() string {
+	return string(p)
+}
+
+func (p RESTPrefix) IsPrefix(line string) bool {
+	return strings.HasPrefix(line, p.String())
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -26,7 +42,46 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+	// Create a buffer for reading lines
+	// go func(conn net.Conn) {
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from connection:", err.Error())
+			os.Exit(1)
+		}
+
+		if arrayPrefix.IsPrefix(line) {
+			fmt.Println("Skipping RESP array length indicator:", line)
+			// Skip RESP array length indicator
+			line, err = reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading from connection:", err.Error())
+				os.Exit(1)
+			}
+			fmt.Println("Skipping RESP bulk string length:", line)
+			// Skip RESP bulk string length
+			line, err = reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading from connection:", err.Error())
+				os.Exit(1)
+			}
+		}
+		fmt.Printf("Received command: %s\n", line)
+		// switch command := strings.TrimSpace(line);
+		command := strings.TrimSpace(line)
+
+		fmt.Printf("Received PING command, command length: %d, command: %s\n", len(command), command)
+		switch strings.ToUpper(command) {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		default:
+			conn.Write([]byte("-ERR unknown command\r\n"))
+		}
+	}
+	// }(conn)
 }
